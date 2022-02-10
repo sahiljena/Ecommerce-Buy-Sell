11d2e5db0d9fd101587a7a299f6064c47b778fe5
sahiljena <sahiljena@protonmail.com>
2022-02-10 13:06:42 +0530

    Added auth-backend

--- test.py
+++
@@ -1,5 +0,0 @@
-import requests 
-
-r = requests.post("https://freeimage.host/api/1/upload",files = {'source': open('test_img.png', 'rb')},data={'key':'6d207e02198a847aa98d0a2a901485a5','action':'upload'})
-print(r.status_code)
-print(r.text)
