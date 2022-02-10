a521df4c3bd86c73f5873b35887c62507ed53c20
sahiljena <sahiljena@protonmail.com>
2022-02-10 16:52:57 +0530

    Fixed bycryptjs

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const multer = require('multer');
 
