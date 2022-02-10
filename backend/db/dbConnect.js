111206569f4aae4724185d58418178e3c0879de1
sahiljena <sahiljena@protonmail.com>
2022-02-10 14:50:54 +0530

    Deploy backend to vercel

--- backend/db/dbConnect.js
+++ backend/db/dbConnect.js
@@ -1,7 +1,7 @@
 // external imports
 const mongoose = require("mongoose");
 require('dotenv').config()
-
+console.log(process.env.DB_URL);
 async function dbConnect() {
   // use mongoose to connect this app to our database on mongoDB using the DB_URL (connection string)
   mongoose
@@ -11,7 +11,6 @@ async function dbConnect() {
         //   these are options to ensure that the connection is done properly
         useNewUrlParser: true,
         useUnifiedTopology: true,
-        useCreateIndex: true,
       }
     )
     .then(() => {
